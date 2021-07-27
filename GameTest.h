280cdb9147f0bbd667facaa8fea9b8af0cb56d8f
Pavel Sarlov <sarlovpavel@gmail.com>
2021-07-27 11:40:40 +0300

    add circle algorithm

--- GameTest.h
+++ GameTest.h
@@ -1,15 +1,14 @@
 #include "PrimitiveEngine.h"
 
+
+
 class GameTest : public PrimitiveEngine
 {
 public:
 
 	void OnUserChange()
 	{
-		COORD x1, y1, x2, y2, x3, y3;
-		scanf_s("%d %d %d %d %d %d", &x1, &y1, &x2, &y2, &x3, &y3);
-
-		FillTriangle(x1, y1, x2, y2, x3, y3, RGB(0xFF, 0xFF, 0xFF));
+		
 	}
 
 	void UserInit()
--- PrimitiveEngine.cpp
+++ PrimitiveEngine.cpp
@@ -29,7 +29,7 @@ ECODE PrimitiveEngine::Run()
 	wc.lpfnWndProc = PrimitiveEngine::ProcessMessages;
 
 	ATOM wClass = RegisterClassEx(&wc);
-	if (!wClass)
+	if(!wClass)
 	{
 		fprintf(stderr, "%ld\n", GetLastError());
 		fprintf(stderr, "%s\n", "Failed to create window class");
@@ -40,7 +40,7 @@ ECODE PrimitiveEngine::Run()
 		WS_OVERLAPPED | WS_VISIBLE | WS_SYSMENU | WS_MINIMIZE | WS_CAPTION | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_SIZEBOX,
 		CW_USEDEFAULT, CW_USEDEFAULT, width, height,
 		nullptr, nullptr, nullptr, nullptr);
-	if (!hwnd)
+	if(!hwnd)
 	{
 		fprintf(stderr, "%ld\n", GetLastError());
 		fprintf(stderr, "%s\n", "Failed to create window");
@@ -55,9 +55,9 @@ ECODE PrimitiveEngine::Run()
 
 	// window loop
 	MSG msg{};
-	while (globalRunning)
+	while(globalRunning)
 	{
-		if (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
+		if(PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
 		{
 			TranslateMessage(&msg);
 			DispatchMessageW(&msg);
@@ -71,7 +71,7 @@ ECODE PrimitiveEngine::Run()
 
 LRESULT CALLBACK PrimitiveEngine::ProcessMessages(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	switch (msg)
+	switch(msg)
 	{
 	case WM_CLOSE:
 		DestroyWindow(hwnd);
@@ -86,7 +86,7 @@ LRESULT CALLBACK PrimitiveEngine::ProcessMessages(HWND hwnd, UINT msg, WPARAM wP
 
 ECODE PrimitiveEngine::DrawPixel(COORD x, COORD y, COLORREF c)
 {
-	if (!SetPixel(this->hdc, x, y, c))
+	if(!SetPixel(this->hdc, x, y, c))
 	{
 		return 1;
 	}
@@ -99,9 +99,9 @@ ECODE PrimitiveEngine::DrawLine(COORD x1, COORD y1, COORD x2, COORD y2, COLORREF
 		dy = y2 - y1,
 		a, b, p;
 
-	if (abs(dx) >= abs(dy))
+	if(abs(dx) >= abs(dy))
 	{
-		if (x1 > x2)
+		if(x1 > x2)
 		{
 			std::swap(x1, x2);
 			std::swap(y1, y2);
@@ -110,7 +110,7 @@ ECODE PrimitiveEngine::DrawLine(COORD x1, COORD y1, COORD x2, COORD y2, COLORREF
 		}
 
 		COORD yi = 1;
-		if (dy < 0)
+		if(dy < 0)
 		{
 			yi = -1;
 			dy = -dy;
@@ -120,14 +120,14 @@ ECODE PrimitiveEngine::DrawLine(COORD x1, COORD y1, COORD x2, COORD y2, COLORREF
 		b = 2 * dx;
 		p = a - dx;
 
-		for (COORD x = x1 + 1, y = y1; x <= x2; x++)
+		for(COORD x = x1 + 1, y = y1; x <= x2; x++)
 		{
-			if (DrawPixel(x, y, c))
+			if(DrawPixel(x, y, c))
 			{
 				return 1;
 			}
 
-			if (p >= 0)
+			if(p >= 0)
 			{
 				y += yi;
 				p -= b;
@@ -137,7 +137,7 @@ ECODE PrimitiveEngine::DrawLine(COORD x1, COORD y1, COORD x2, COORD y2, COLORREF
 	}
 	else
 	{
-		if (y1 > y2)
+		if(y1 > y2)
 		{
 			std::swap(x1, x2);
 			std::swap(y1, y2);
@@ -146,7 +146,7 @@ ECODE PrimitiveEngine::DrawLine(COORD x1, COORD y1, COORD x2, COORD y2, COLORREF
 		}
 
 		COORD xi = 1;
-		if (dx < 0)
+		if(dx < 0)
 		{
 			xi = -1;
 			dx = -dx;
@@ -156,14 +156,14 @@ ECODE PrimitiveEngine::DrawLine(COORD x1, COORD y1, COORD x2, COORD y2, COLORREF
 		b = 2 * dy;
 		p = a - dy;
 
-		for (COORD x = x1, y = y1 + 1; y <= y2; y++)
+		for(COORD x = x1, y = y1 + 1; y <= y2; y++)
 		{
-			if (DrawPixel(x, y, c))
+			if(DrawPixel(x, y, c))
 			{
 				return 1;
 			}
 
-			if (p >= 0)
+			if(p >= 0)
 			{
 				x += xi;
 				p -= b;
@@ -177,7 +177,7 @@ ECODE PrimitiveEngine::DrawLine(COORD x1, COORD y1, COORD x2, COORD y2, COLORREF
 
 ECODE PrimitiveEngine::DrawTriangle(COORD x1, COORD y1, COORD x2, COORD y2, COORD x3, COORD y3, COLORREF c)
 {
-	if (DrawLine(x1, y1, x2, y2, c) ||
+	if(DrawLine(x1, y1, x2, y2, c) ||
 		DrawLine(x2, y2, x3, y3, c) ||
 		DrawLine(x3, y3, x1, y1, c))
 	{
@@ -189,11 +189,57 @@ ECODE PrimitiveEngine::DrawTriangle(COORD x1, COORD y1, COORD x2, COORD y2, COOR
 
 ECODE PrimitiveEngine::DrawCircle(COORD x1, COORD y1, COORD r, COLORREF c)
 {
+	COORD e = -r,
+		x = r,
+		y = 0;
+
+	while(y <= x)
+	{
+		DrawPixel(x1 + x, y1 + y, c);
+		DrawPixel(x1 - x, y1 + y, c);
+		DrawPixel(x1 + x, y1 - y, c);
+		DrawPixel(x1 - x, y1 - y, c);
+		DrawPixel(x1 + y, y1 + x, c);
+		DrawPixel(x1 - y, y1 + x, c);
+		DrawPixel(x1 + y, y1 - x, c);
+		DrawPixel(x1 - y, y1 - x, c);
+
+		e += 2 * y + 1;
+		y++;
+
+		if(e >= 0)
+		{
+			e -= 2 * x - 1;
+			x--;
+		}
+	}
+
 	return 0;
 }
 
 ECODE PrimitiveEngine::FillCircle(COORD x1, COORD y1, COORD r, COLORREF c)
 {
+	COORD e = -r,
+		x = r,
+		y = 0;
+
+	while(y <= x)
+	{
+		DrawLine(x1 - x, y1 + y, x1 + x, y1 + y, c);
+		DrawLine(x1 - x, y1 - y, x1 + x, y1 - y, c);
+		DrawLine(x1 - y, y1 + x, x1 + y, y1 + x, c);
+		DrawLine(x1 - y, y1 - x, x1 + y, y1 - x, c);
+
+		e += 2 * y + 1;
+		y++;
+
+		if(e >= 0)
+		{
+			e -= 2 * x - 1;
+			x--;
+		}
+	}
+
 	return 0;
 }
 
@@ -209,19 +255,19 @@ ECODE PrimitiveEngine::FillTriangle(COORD x1, COORD y1, COORD x2, COORD y2, COOR
 		curY3 = y1,
 		a2, b2, a3, b3, p2, p3, yi2 = 1, yi3 = 1;
 
-	if (dy2 < 0)
+	if(dy2 < 0)
 	{
 		yi2 = -1;
 		dy2 = -dy2;
 	}
 
-	if (dy3 < 0)
+	if(dy3 < 0)
 	{
 		yi3 = -1;
 		dy3 = -dy3;
 	}
 
-	if (abs(dx2) >= abs(dy2))
+	if(abs(dx2) >= abs(dy2))
 	{
 		a2 = 2 * dy2;
 		b2 = 2 * dx2;
@@ -234,7 +280,7 @@ ECODE PrimitiveEngine::FillTriangle(COORD x1, COORD y1, COORD x2, COORD y2, COOR
 		p2 = a2 - dy2;
 	}
 
-	if (abs(dx3) >= abs(dy3))
+	if(abs(dx3) >= abs(dy3))
 	{
 		a3 = 2 * dy3;
 		b3 = 2 * dx3;
@@ -247,26 +293,26 @@ ECODE PrimitiveEngine::FillTriangle(COORD x1, COORD y1, COORD x2, COORD y2, COOR
 		p3 = a3 - dy3;
 	}
 
-	for (COORD x = x1; x <= x3; x++)
+	for(COORD x = x1; x <= x3; x++)
 	{
-		if (DrawLine(x, curY2, x, curY3, c))
+		if(DrawLine(x, curY2, x, curY3, c))
 		{
 			return 1;
 		}
 
-		if (x == x2 && curY2 == y2)
+		if(x == x2 && curY2 == y2)
 		{
 			dx2 = x3 - x2;
 			dy2 = y3 - y2;
 			yi2 = 1;
 
-			if (dy2 < 0)
+			if(dy2 < 0)
 			{
 				yi2 = -1;
 				dy2 = -dy2;
 			}
 
-			if (abs(dx2) >= abs(dy2))
+			if(abs(dx2) >= abs(dy2))
 			{
 				a2 = 2 * dy2;
 				b2 = 2 * dx2;
@@ -280,11 +326,11 @@ ECODE PrimitiveEngine::FillTriangle(COORD x1, COORD y1, COORD x2, COORD y2, COOR
 			}
 		}
 
-		if (abs(dx2) >= abs(dy2))
+		if(abs(dx2) >= abs(dy2))
 		{
-			if (p2 >= 0)
+			if(p2 >= 0)
 			{
-				while (p2 >= 0)
+				while(p2 >= 0)
 				{
 					curY2 += yi2;
 					p2 -= b2 - a2;
@@ -297,14 +343,14 @@ ECODE PrimitiveEngine::FillTriangle(COORD x1, COORD y1, COORD x2, COORD y2, COOR
 		}
 		else
 		{
-			if (p2 < 0)
+			if(p2 < 0)
 			{
 				curY2 += yi2;
 				p2 += a2;
 			}
 			else
 			{
-				while (p2 >= 0)
+				while(p2 >= 0)
 				{
 					curY2 += yi2;
 					p2 -= b2 - a2;
@@ -312,11 +358,11 @@ ECODE PrimitiveEngine::FillTriangle(COORD x1, COORD y1, COORD x2, COORD y2, COOR
 			}
 		}
 
-		if (abs(dx3) >= abs(dy3))
+		if(abs(dx3) >= abs(dy3))
 		{
-			if (p3 >= 0)
+			if(p3 >= 0)
 			{
-				while (p3 >= 0)
+				while(p3 >= 0)
 				{
 					curY3 += yi3;
 					p3 -= b3 - a3;
@@ -329,14 +375,14 @@ ECODE PrimitiveEngine::FillTriangle(COORD x1, COORD y1, COORD x2, COORD y2, COOR
 		}
 		else
 		{
-			if (p3 < 0)
+			if(p3 < 0)
 			{
 				curY3 += yi3;
 				p3 += a3;
 			}
 			else
 			{
-				while (p3 >= 0)
+				while(p3 >= 0)
 				{
 					curY3 += yi3;
 					p3 -= b3 - a3;
@@ -350,19 +396,19 @@ ECODE PrimitiveEngine::FillTriangle(COORD x1, COORD y1, COORD x2, COORD y2, COOR
 
 void PrimitiveEngine::SortLeftRightTriangle(COORD& x1, COORD& y1, COORD& x2, COORD& y2, COORD& x3, COORD& y3)
 {
-	if (x1 > x2 || (x1 == x2 && y1 > y2))
+	if(x1 > x2 || (x1 == x2 && y1 > y2))
 	{
 		std::swap(x1, x2);
 		std::swap(y1, y2);
 	}
 
-	if (x1 > x3 || (x1 == x3 && y1 > y3))
+	if(x1 > x3 || (x1 == x3 && y1 > y3))
 	{
 		std::swap(x1, x3);
 		std::swap(y1, y3);
 	}
 
-	if (x2 > x3 || (x2 == x3 && y2 > y3))
+	if(x2 > x3 || (x2 == x3 && y2 > y3))
 	{
 		std::swap(x2, x3);
 		std::swap(y2, y3);
